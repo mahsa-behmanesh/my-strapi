28fde0d826903f1fadd391f1d8223d4985a319de
yaser zarvandi <mahsa.behmanesh74@gmail.com>
2025-05-24 10:36:43 +0330

    feat(): add api config

---
+++ src/config/api.ts
@@ -0,0 +1,10 @@
+export default ({ env }) => ({
+  responses: {
+    privateAttributes: ["_v", "id", "created_at"],
+  },
+  rest: {
+    prefix: "/v1",
+    defaultLimit: 100,
+    maxLimit: 250,
+  },
+});
